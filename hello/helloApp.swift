11007e804542cd27435e760955e26b9d16cff985
Jihunkim95 <darak551@naver.com>
2023-09-19 09:59:39 +0900

    Initial Commit

---
+++ hello/helloApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloApp.swift
+//  hello
+//
+//  Created by 김지훈 on 2023/09/19.
+//
+
+import SwiftUI
+
+@main
+struct helloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
